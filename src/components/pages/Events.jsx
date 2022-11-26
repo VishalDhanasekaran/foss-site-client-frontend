3790be6eeb908c6eaa420af47b3322eccfd3e658
Vignaraj-1018 <86393989+Vignaraj-1018@users.noreply.github.com>
2022-11-26 13:48:32 +0530

    events page

--- src/components/pages/Events.jsx
+++ src/components/pages/Events.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import styles from '../../style';
+import GetStarted from '../GetStarted';
 
 const Events = () => {
   return (
-    <div>Events</div>
+    <div className={`flex flex-col ${styles.paddingY}`}>
+      <section id={'community'} className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <h2 className="flex-1 font-poppins font-semibold ss:text-[42px] text-[22px] text-white ss:leading-[100.8px] leading-[75px]">
+          Join Our Community
+        </h2>
+        <GetStarted text={'Telegram'}/>
+      </section>
+      <section id={'gallery'} className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+        <h2 className="flex-1 font-poppins font-semibold ss:text-[42px] text-[22px] text-white ss:leading-[100.8px] leading-[75px]">
+          Events Gallery
+        </h2>
+        <p>
+          Events Picture can be displayed here...
+        </p>
+      </section>
+    </div>
   )
 }
 
