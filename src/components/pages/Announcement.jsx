ca9581ec0a54ab349a7552b5efdbe7297d9464c5
Vignaraj-1018 <86393989+Vignaraj-1018@users.noreply.github.com>
2022-11-27 12:49:16 +0530

    init

--- src/components/pages/Announcement.jsx
+++ src/components/pages/Announcement.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import styles from '../../style'
 
 const Announcement = () => {
   return (
-    <div>Announcement</div>
+    <div className={`flex flex-col ${styles.paddingY} h-screen`}>
+      <h1 className="flex-1 font-poppins text-center font-semibold ss:text-[52px] text-[32px] text-white ss:leading-[100.8px] leading-[75px]">
+          Announcements
+      </h1>
+    </div>
+
   )
 }
 
